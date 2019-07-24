ff99836651e6e63edff32f28e185f47c0ba956d4
Charles <charleshu32@gmail.com>
2019-07-24 00:45:49 -0400

    One type to make some audio

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
+   
     
+    var player:AVAudioPlayer?;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +21,16 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+         let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!;
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else {return};
+            
+            player.prepareToPlay();
+            player.play();
+        } catch let error as Error{
+            print(error);
+        }
     }
     
   

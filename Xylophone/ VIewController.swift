9482ebdf8c5c849d54ef631743a9cd190597d911
Charles <charleshu32@gmail.com>
2019-10-02 18:24:10 -0400

    Change it to array

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -14,8 +14,8 @@ import AVFoundation
 class ViewController: UIViewController{
    
     
-    var player:AVAudioPlayer!
-
+    var player:AVAudioPlayer!;
+    let soundAudioArray=["note1","note2","note3","note4","note5","note6","note7"];
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -23,6 +23,9 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        var selectedSoundFileName : String = soundAudioArray[sender.tag - 1];
+        print(selectedSoundFileName)
+        playsound();
 //        let key = String(sender.tag)
 //        if let soundURL = Bundle.main.url(forResource:"note\(key)", withExtension: "wav"){
 //            var mySound:SystemSoundID = 0;
@@ -43,3 +46,14 @@ class ViewController: UIViewController{
 //        }
     }
 
+    func playsound(){
+         let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!;
+               do{
+                    player = try AVAudioPlayer(contentsOf: url)
+                } catch{
+                    print(error);
+                }
+              player.play();
+    }
+
+}
